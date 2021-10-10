67b804a72d3fad8bba6b3b3e1f5cdd79dcbabcd0
Evgenia Kurineva <kurineva85@mail.ru>
2021-10-10 22:45:46 +0300

    feat: add phone validation

--- museum-dom/js/formValidation.js
+++ museum-dom/js/formValidation.js
@@ -1,5 +1,6 @@
 const nameInput = document.querySelector('.input-name');
 const emailInput = document.querySelector('.input-email');
+const telInput = document.querySelector('.input-tel');
 
 /* ******************* */
 const addError = (element, text) => {
@@ -132,8 +133,91 @@ const customEmailValidate = () => {
 };
 
 /* ******************* */
+const customTelValidate = () => {
+  // валидация номера телефона.
+  // Номер телефона должен содержать не больше 10 цифр.
+  // Цифры могут быть без разделения или с разделением на две или три цифры.
+  // Разделение цифр может быть через дефис или пробел.
+
+  const value = telInput.value;
+
+  if (value === '') {
+    addError(telInput, 'Enter your phone!');
+    return;
+  }
+
+  //   const countOfDigits = parseInt(value.replace(/\D+/g, '')).toString().length;
+  const countOfDigits = value.match(/\d/g).length;
+
+  if (
+    // Номер телефона должен содержать не больше 10 цифр.
+    countOfDigits < 6 ||
+    countOfDigits > 10
+  ) {
+    addError(
+      telInput,
+      'Phone number must be more than 6 and no more than 10 digits',
+    );
+    return;
+  }
+
+  const separatorMacth = value.match(/\D+/);
+
+  if (
+    // Цифры могут быть без разделения.
+    !separatorMacth
+  ) {
+    removeError(telInput);
+    return;
+  }
+
+  if (
+    // Разделение цифр может быть через пробел или дефис.
+    separatorMacth &&
+    !/^([\d\s]+|[\d\-]+)$/.test(value)
+  ) {
+    addError(telInput, 'Numbers can be separated only by a hyphen or a space.');
+    return;
+  }
+
+  const separator = separatorMacth[0];
+
+  const valueArray = value.split(separator);
+  const isSeparateValid = valueArray.every(
+    (item) => item.length === 2 || item.length === 3,
+  );
+
+  if (
+    // Цифры могут быть с разделением на две или три цифры.
+    !isSeparateValid
+  ) {
+    addError(
+      telInput,
+      'The numbers can be without division or with division into two or three digits.',
+    );
+    return;
+  }
+
+  removeError(telInput);
+};
+
+/* ******************* */
+const preventEvent = (event) => {
+  if (event.code === 'Enter') {
+    console.log(123);
+    formSubmitHandler(event);
+  }
+};
 
 /* ******************* */
 nameInput.addEventListener('blur', customNameValidate);
 
 emailInput.addEventListener('blur', customEmailValidate);
+
+telInput.addEventListener('blur', customTelValidate);
+
+nameInput.addEventListener('keyup', preventEvent);
+
+emailInput.addEventListener('keyup', preventEvent);
+
+telInput.addEventListener('keyup', preventEvent);
